6f3989a997e3147c76ec78ceedb976536314ca60
Tim Leslie <timl@thinkmill.com.au>
2021-03-04 14:37:34 +1100

    [WIP] Add GraphQL API docs

--- examples-next/todo/keystone.ts
+++ examples-next/todo/keystone.ts
@@ -1,34 +1,12 @@
-import { config } from '@keystone-next/keystone/schema';
-import { statelessSessions, withItemData } from '@keystone-next/keystone/session';
-import { lists } from './schema';
-import { createAuth } from '@keystone-next/auth';
+import { config, createSchema, list } from '@keystone-next/keystone/schema';
+import { text } from '@keystone-next/fields';
 
-const sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
-const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
-const sessionConfig = {
-  maxAge: sessionMaxAge,
-  secret: sessionSecret,
-};
-
-const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  initFirstItem: {
-    fields: ['name', 'email', 'password'],
+export default config({
+  db: {
+    adapter: 'prisma_postgresql',
+    url: 'postgres://keystone5:k3yst0n3@localhost:5432/todo-example',
   },
+  lists: createSchema({
+    User: list({ fields: { name: text() } }),
+  }),
 });
-
-export default withAuth(
-  config({
-    db: {
-      adapter: 'prisma_postgresql',
-      url: 'postgres://keystone5:k3yst0n3@localhost:5432/todo-example',
-    },
-    lists,
-    ui: {
-      isAccessAllowed: ({ session }) => !!session,
-    },
-    session: withItemData(statelessSessions(sessionConfig)),
-  })
-);
